d81eff88b320a5c09ab880323e1f3f009450eae5
vasco <vastur@gmail.com>
2017-08-15 00:04:40 -0700

    Refactor out unneeded D3

--- src/fit-to-box.js
+++ src/fit-to-box.js
@@ -1,5 +1,4 @@
 import Kapsule from 'kapsule';
-import * as d3 from 'd3';
 
 export default Kapsule({
     props: {
@@ -7,17 +6,17 @@ export default Kapsule({
         passes: { default: 3 }
     },
     init(el, state) {
-        state.el = d3.select(el);
+        state.el = el;
     },
     update(state) {
-        d3.range(state.passes).some(() => {
-            const startSize = parseInt(state.el.style('font-size').split('px')[0]);
-            const bbox = state.el.node().getBBox();
+        [...Array(state.passes).keys()].some(() => {
+            const startSize = parseInt(state.el.style['font-size'].split('px')[0]);
+            const bbox = state.el.getBBox();
             const newSize = Math.floor(startSize * Math.min(state.bbox.width / bbox.width, state.bbox.height / bbox.height));
 
             if (newSize === startSize) return true; // Shortcut out
 
-            state.el.style('font-size', newSize + 'px');
+            state.el.style['font-size'] = newSize + 'px';
         });
     }
 });
